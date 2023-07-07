6f5e8e31cdd6d4d6107ad6eedc09d14f6bc77c9d
S.P <s.protsenk@gmail.com>
2023-07-07 19:48:43 +0700

    s03 added

---
+++ src/study_group/Main.java
@@ -0,0 +1,21 @@
+package study_group;
+
+public class Main {
+    public static void main(String[] args) {
+        Service service = new Service();
+
+        service.addStudent("Stud5", 18);
+        service.addStudent("Stud4", 16);
+        service.addStudent("Stud2", 17);
+        service.addStudent("Stud4", 18);
+        service.addStudent("Stud6", 19);
+        service.addStudent("Stud1", 29);
+
+        System.out.println(service.getStudentsInfo());
+        service.sortByName();
+        System.out.println(service.getStudentsInfo());
+        service.sortByAge();
+        System.out.println(service.getStudentsInfo());
+
+    }
+}
---
+++ src/study_group/Service.java
@@ -0,0 +1,46 @@
+package study_group;
+
+import study_group.group.StudyGroup;
+import study_group.student.Student;
+
+public class Service {
+    private long id;
+    private StudyGroup studyGroup;
+
+    public Service() {
+        this.studyGroup = new StudyGroup();
+    }
+
+    public void addStudent(String name, int age) {
+
+        Student student = new Student(id++, name, age);
+        studyGroup.addStudent(student);
+    }
+
+    public String getStudentsInfo() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Students:\n");
+
+//                  foreach loop
+//        Iterator<Student> iterator = studyGroup.iterator();
+//        while (iterator.hasNext()) {
+//            Student student = iterator.next();
+//            sb.append(student);
+//            sb.append("\n");
+//        }
+
+        for (Student student : studyGroup) {
+            sb.append(student);
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    public void sortByName() {
+        studyGroup.sortByName();
+    }
+
+    public void sortByAge() {
+        studyGroup.sortByAge();
+    }
+}
